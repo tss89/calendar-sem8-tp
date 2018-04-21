cd9ed5d8ffcf709557f1d2e0a30ded4aa2a1ff35
Aymaro <lodrigos123@gmail.com>
2018-04-21 10:05:43 +0200

    database connection setting added

--- calendar_sem8/settings.py
+++ calendar_sem8/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'calendar_sem8.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql', 
+        'NAME': 'aymaropl_callendar',
+        'USER': 'aymaropl_callendar_admin',
+        'PASSWORD': '8HJA.1MQeVV@',
+        'HOST': 'aymaro.pl',
+        'PORT': '3306',
     }
 }
 
