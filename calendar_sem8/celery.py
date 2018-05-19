fbf4cce6d5076d8c3e02bed728066df99a4eb3fb
Tomasz Sobanski <tomasz.sobanski@venturedevs.com>
2018-05-19 09:58:44 +0200

    celery install

---
+++ calendar_sem8/celery.py
@@ -0,0 +1,23 @@
+from __future__ import absolute_import, unicode_literals
+
+import os
+from celery import Celery
+from django.conf import settings
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'calendar_sem8.settings')
+
+celery_app = Celery('proj')
+
+# Using a string here means the worker doesn't have to serialize
+# the configuration object to child processes.
+# - namespace='CELERY' means all celery-related configuration keys
+#   should have a `CELERY_` prefix.
+celery_app.config_from_object('django.conf:settings', namespace='CELERY')
+
+# Load task modules from all registered Django app configs.
+celery_app.autodiscover_tasks()
+
+celery_app.conf.update(result_expires=3600)
+
+if __name__ == '__main__':
+    celery_app.start()
--- calendar_sem8/settings.py
+++ calendar_sem8/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'users',
     'friends',
     'sslserver',
+    'celery',
 ]
 
 MIDDLEWARE = [
